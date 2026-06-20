69af28ecc7bc24b16088c1c5ca14ac7014d17934
Nitin Pangal <nitinpangal1984@gmail.com>
2026-06-20 09:47:32 +0530

    Initial Next.js application

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Hello from Nitin's First Vercel App</h1>
+      <p>Learning GitHub and Vercel Deployment</p>
+    </main>
+  );
+}
